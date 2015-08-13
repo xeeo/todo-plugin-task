09d88075c8e9bf47ad519e50379a1da67bf6fe53
xeeo <geana.raul@gmail.com>
2015-08-13 12:50:22 +0300

    updated entrypoint to match hapi 8

--- src/entrypoints.js
+++ src/entrypoints.js
@@ -5,36 +5,19 @@ module.exports = function (server, plugin) {
     server.route({
         method: 'GET',
         path: '/task/showText',
-        config : {
-            handler : plugin.showText
-        }
+        handler : plugin.showText,
+        config : {}
     });
 
     server.route({
         method: ['POST', 'PUT'],
         path: '/task/create',
-        config : {
-            handler : function(request, reply) {
-
-                setTimeout(function(review) {
-
-                    reply('this is the user');
-
-                    sendMessage('inform-indexer-about-review', {'review':review});
-                    sendMessage('inform-elastic-search', {});
-                    sendMessage('created-review', {});
-
-
-                }, 3000);
-
-
-            }
-        }
+        handler : plugin.create,
+        config : {}
     });
 
     //rabbit.on('pam pam', function (params) {
     //    plugin.doSomething(params);
     //});
-
 
 };
